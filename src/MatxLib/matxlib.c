aefb8057d872a1ecf8a93a57d21d4ece58d9a7c7
Cifer-ti <draculemihawk232@gmail.com>
2023-10-20 22:28:44 +0100

    removed test lines

--- src/MatxLib/matxlib.c
+++ src/MatxLib/matxlib.c
@@ -40,6 +40,8 @@ void eval(int row, int col, int matx[row][col], int res[row][col], char op)
     }
 }
 
+
+
 char *parseMatrix(char* str, int col, int row, int matrix[row][col])
 {
     char *s = str;
@@ -68,7 +70,8 @@ int main(void)
     char input[MAX_TOKEN_LEN];
     char operator[MAX_TOKEN_LEN / 2];
     char *p = '\0';
-    int i = 0, col, row, ind = 0;
+    int i = 0, j = 0;
+    int col, row, ind = 0;
 
     printf("Enter matrix dimensions(mxn): ");
     scanf("%dx%d", &row, &col);
@@ -82,79 +85,39 @@ int main(void)
             rest[i][j] = 0;
     }
     i = 0;
+
     printf("Enter matrix expression: ");
     fgets(input, sizeof(input), stdin);
 
-    /*****************
-     * remove the new line character
-     *****************
-     */
-    //input[strcspn(input, "\n")] = '\0';
-    //printf("yeah\n");
-
-    /***************
-     * mark all operations and store them in some sort of queue
-     * **************
-     * p = strpbrk(input, "+-=");
-     * operator[i++] = *P;
-     * for(i = 0;; *p != NULL; p++) {
-     *   p = strpbrk(p + 1, "+-=");   
-     *   operator[i] = *p;
-     * }
-     */
-    printf("str: %s\n", input);
+    /**
+     *  since operators seperate differnt matrixes, use
+     * replace it with '\0' so they can be as seperate strigs
+    */
     p = strpbrk(input, "+-*=");
     operator[i++] = *p;
-    printf("strpbk: %c\n", *p);
     *p = '\0';
+
     while(1) {
         p = strpbrk(p + 1, "+-=*"); 
         if( p == NULL)
             break;
-        printf("strpbk: %c\n", *p);  
+  
         operator[i++] = *p;
         *p = '\0';
     }
     operator[i] = '\0';
-    printf("ope: %s", operator);
-    printf("str: %s\n", input);
-    p = parseMatrix(input, col, row, matx);
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j < col; j++)
-            printf("%d ", matx[i][j]);
-        printf("\n");
-    }
 
-    printf("\n\nfirst addition\n\n");
+    
+    p = parseMatrix(input, col, row, matx);
     addmatx(row, col, matx, rest);
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j < col; j++)
-            printf("%d ", rest[i][j]);
-        printf("\n");
-    }
+
+
     while(*p != '\n') {
         if(p + 1 != NULL) {
             p = parseMatrix(p, col, row, matx);
-            eval(row, col, matx, rest, operator[ind]);
-            ind++;
-            for(int i = 0; i < row; i++) {
-                for(int j = 0; j < col; j++)
-                    printf("%d ", matx[i][j]);
-                printf("\n");
-            }
+            eval(row, col, matx, rest, operator[ind++]);
         }
     }
 
-    printf("\n\nsecond addition\n\n");
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j < col; j++)
-            printf("%d ", rest[i][j]);
-        printf("\n");
-    }
-
-    
-
     return 1;
-
-   /* use strtok function to start tokenizing the string */
 } 
