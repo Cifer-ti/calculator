5fc7570eb1aa667ecb930c1298ddeb527756d9d6
Cifer-ti <draculemihawk232@gmail.com>
2023-11-21 23:31:29 +0100

    Add the operation routines
    Added the functions that support addition, subtraction,
    multiplication and division.

--- src/complib/complib.c
+++ src/complib/complib.c
@@ -15,15 +15,18 @@ typedef enum {
     ERR,
 }OperationChoice;
 
-ComplexNumber c1, c2;
-
 OperationChoice Printoptions(void);
-void getComplexnum(void);
+void getComplexnum(ComplexNumber *z1, ComplexNumber *z2);
+void complexAddition(ComplexNumber z1, ComplexNumber z2, ComplexNumber *ans);
+void complexSubtraction(ComplexNumber z1, ComplexNumber z2, ComplexNumber *ans);
+void complexMultiplication(ComplexNumber z1, ComplexNumber z2, ComplexNumber *ans);
+void complexDivision(ComplexNumber z1, ComplexNumber z2, ComplexNumber *ans);
 
 int main() {
     char sign; // To store the '+' or '-' sign
     char i; // To store the 'i' character
     int choice;
+    ComplexNumber c1, c2, ans;
 
     printf("\n*** Simple complex number calculator ***\n\n");
     printf("*** Numbers should be entered in format 'a + bi'. ***\n"
@@ -38,26 +41,29 @@ int main() {
         switch(choice) {
             case addition:
                 printf("a\n");
-                getComplexnum();
+                getComplexnum(&c1, &c2);
+                complexAddition(c1, c2, &ans);
                 //call addition routine
                 break;
             
             case subtraction:
                 printf("s\n");
-                getComplexnum();
+                getComplexnum(&c1, &c2);
+                complexSubtraction(c1, c2, &ans);
                 //call subtraction routine
                 break;
             
             case multiplication:
                 printf("m\n");
-                getComplexnum();
-
+                getComplexnum(&c1, &c2);
+                complexMultiplication(c1, c2, &ans);
                 //call multiplication routine
                 break;
             
             case division:
                 printf("d\n");
-                getComplexnum();
+                getComplexnum(&c1, &c2);
+                complexDivision(c1, c2, &ans);
                 //call division routine
                 break;
             
@@ -69,6 +75,7 @@ int main() {
                 printf("er\n");
                 continue;
         }
+        printf("Answer = %.2lf + %.2lfi\n", ans.real, ans.imaginary);
     }while(1);
 
     return 0;
@@ -81,24 +88,54 @@ OperationChoice Printoptions(void)
     printf("1- ADDITION\n 2- SUBTRACTION\n 3- MULTIPLICATION\n 4- DIVISION\n 0- EXIT\n");
 }
 
-void getComplexnum(void)
+void getComplexnum(ComplexNumber *z1, ComplexNumber *z2)
 {   
     char i, sign;
 
     printf("Enter first complex number: ");
-    if(scanf("%lf %c %lf %c", &c1.real, &sign, &c1.imaginary, &i) != 4) {
+    if(scanf("%lf %c %lf %c", &z1->real, &sign, &z1->imaginary, &i) != 4) {
         fprintf(stderr, "Wrong format\n");
     }
 
     if(sign == '-')
-        c1.imaginary = -c1.imaginary;
+        z1->imaginary = -z1->imaginary;
 
     printf("Enter second complex number: ");
-   if(scanf("%lf %c %lf %c", &c2.real, &sign, &c2.imaginary, &i) != 4) {
+   if(scanf("%lf %c %lf %c", &z2->real, &sign, &z2->imaginary, &i) != 4) {
         fprintf(stderr, "Wrong format\n");
     }
 
     if(sign == '-')
-        c2.imaginary = -c2.imaginary;
+        z2->imaginary = -z2->imaginary;
 
+}
+
+void complexAddition(ComplexNumber z1, ComplexNumber z2, ComplexNumber *ans)
+{
+    ans->real = z1.real + z2.real;
+    ans->imaginary = z1.imaginary + z2.imaginary;
+}
+
+void complexSubtraction(ComplexNumber z1, ComplexNumber z2, ComplexNumber *ans)
+{
+    ans->real = z1.real - z2.real;
+    ans->imaginary = z1.imaginary - z2.imaginary;
+}
+
+void complexMultiplication(ComplexNumber z1, ComplexNumber z2, ComplexNumber *ans)
+{
+    ans->real = (z1.real * z2.real) - (z1.imaginary * z2.imaginary);
+    ans->imaginary = (z1.real * z2.imaginary) + (z1.imaginary * z2.real);
+}
+
+void complexDivision(ComplexNumber z1, ComplexNumber z2, ComplexNumber *ans)
+{
+    int divisor = (z2.real * z2.real) - (z2.imaginary * z2.imaginary);
+
+    if(divisor == 0) {
+        fprintf(stderr, "Divisor resultsl to zero\n");
+        exit(EXIT_FAILURE);
+    }
+    ans->real = ((z1.real * z2.real) - (z1.imaginary * z2.imaginary)) / divisor;
+    ans->imaginary = ((z1.real * z2.imaginary) + (z1.imaginary * z2.real)) / divisor;
 }
