341d3d402f38c13bbc0d09d63932919b978091a7
Cifer-ti <draculemihawk232@gmail.com>
2023-11-21 10:07:48 +0100

    feat: Add main interface for complex number lib
    Had to change the implementation to the classical menu form
    to reduce complexity. This is just the main interface

--- src/complib/complib.c
+++ src/complib/complib.c
@@ -1,66 +1,77 @@
-/* This is the library that takes care of complex numbers */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 typedef struct {
-    int real;
-    int imaginary;
+    double real;
+    double imaginary;
 } ComplexNumber;
 
+typedef enum {
+    quit,
+    addition,
+    subtraction,
+    multiplication,
+    division,
+    ERR,
+}OperationChoice;
+
+ComplexNumber c1, c2;
+
+OperationChoice Printoptions(void);
+
 int main() {
-    char complexString[100]; // Assuming a maximum input length of 100 characters
-    char* token;
-    char* delimiter = " +-";
-    char operation[100];
-    char* p;
-    int i = 0, j = 0;
-    
-    printf("Enter the complex string: ");
-    fgets(complexString, sizeof(complexString), stdin);
+    char sign; // To store the '+' or '-' sign
+    char i; // To store the 'i' character
+    int choice;
 
-    complexString[strcspn(complexString, "\n")] = '\0';
-    
-    int maxComplexNumbers = 10; // Maximum number of complex numbers to handle
-    ComplexNumber complexNumbers[maxComplexNumbers];
-    int numComplexNumbers = 0;
-
-    p = strpbrk(complexString, "+-");
-    while(p != NULL) {
-        if(i % 2 != 0)
-            operation[j++] = *p;
-        p = strpbrk(p + 1, "+-");
-        i++;
-    }
-    
-    operation[j] = '\0';
-
-    printf("Opetors: %s\n", operation);
-    // Extract complex numbers from the input string
-    token = strtok(complexString, delimiter);
-    while (token != NULL) {
-        complexNumbers[numComplexNumbers].real = atoi(token);
-        token = strtok(NULL, " + -");
+    printf("\n*** Simple complex number calculator ***\n\n");
+    printf("*** Numbers should be entered in format 'a + bi'. ***\n"
+            "# If a complex number lacks a part specify by replacing it with zero #\n\n");
         
-        if (token != NULL && token[strlen(token) - 1] == 'i') {
-            if(strlen(token) == 1) {
-                complexNumbers[numComplexNumbers].imaginary = 1;
-                numComplexNumbers++;
-            }
-            else {
-                token[strlen(token) - 1] = '\0'; // Remove the 'i' character
-                complexNumbers[numComplexNumbers].imaginary = atoi(token);
-                numComplexNumbers++;
-            }
+    Printoptions();
+    do {
+
+        printf("what do you want to do: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case addition:
+                printf("a\n");
+                //call addition routine
+                break;
+            
+            case subtraction:
+                printf("s\n");
+                //call subtraction routine
+                break;
+            
+            case multiplication:
+                printf("m\n");
+
+                //call multiplication routine
+                break;
+            
+            case division:
+                printf("d\n");
+                //call division routine
+                break;
+            
+            case quit:
+                printf("ex\n");
+                exit(EXIT_FAILURE);
+            
+            case ERR:
+                printf("er\n");
+                continue;
         }
-        
-        token = strtok(NULL, delimiter);
-    }
-    
-    // Print the extracted complex numbers
-    for (int i = 0; i < numComplexNumbers; i++) {
-        printf("Complex Number %d: %d + %di\n", i+1, complexNumbers[i].real, complexNumbers[i].imaginary);
-    }
-    
+    }while(1);
+
     return 0;
+
+}
+
+OperationChoice Printoptions(void)
+{
+    printf("****Operations List****\n");
+    printf("1- ADDITION\n 2- SUBTRACTION\n 3- MULTIPLICATION\n 4- DIVISION\n 0- EXIT\n");
 }
