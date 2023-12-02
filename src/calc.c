792e4369169a862c05a0eb0755ceaa83c8f7e3ab
Cifer-ti <draculemihawk232@gmail.com>
2023-12-02 20:37:29 +0100

    change the commandline parser routines library
    I'm changing the implementation of the commandline parser functions
    from using the funcitons int the getopt library to the argp library,
    which has better management.

--- src/calc.c
+++ src/calc.c
@@ -1,9 +1,12 @@
 #include <getopt.h>
+#include <argp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "complib/complib.h"
 #include "matxlib/matxlib.h"
 #include "scilib/scilib.h"
+
+int choice = 0;
 
 typedef enum {
     quit,
@@ -21,6 +24,38 @@ typedef enum {
 }Mode;
 
 void PrintMainBoard(void);
+void dashboard(int *choice);
+
+static struct argp_option options[] = {
+    {"sci", 's', 0, 0, "Call the Scientific library", 0},
+    {"matx", 'm', 0, 0, "Perform matrix operations", 0},
+    {"compx", 'c', 0, 0, "Perform operations on complex numbers", 0},
+    {0}
+};
+
+// Parse the command-line arguments
+static error_t parse_opt(int key, char *arg) {
+    //struct arguments *arguments = state->input;
+
+    switch (key) {
+        case 's':
+            choice = scientific;
+            break;
+        case 'm':
+            choice = matrix;
+            break;
+        case 'c':
+            choice = complex;
+            break;
+        case ARGP_KEY_ARG:
+            // Handle non-option arguments here, if needed
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
@@ -37,43 +72,7 @@ int main(int argc, char **argv)
     printf("%d\n", choiceOfMode);
     if(choiceOfMode == interactive) {
         PrintMainBoard();
-        do {
-            printf("What do you want to do(choose from the above operation codes): ");
-
-            if(scanf("%d", &choice) != 1) { /* consume bad command */
-                while(getchar() != '\n')
-                    ;
-            }
-
-
-            switch(choice) {
-                case 0:
-                    printf("quit\n");
-                    return 0;
-                case 1:
-                    choice = scientific;
-                    printf("scientific lib\n");
-                    break;
-                
-                case 2:
-                    printf("Matrixlib\n");
-                    choice = matrix;
-                    break;
-
-                case 3:
-                    printf("complex lib\n");
-                    choice = complex;
-                    break;
-
-                case 4:
-                    printf("clear\n");
-                    break;
-
-                case 5:
-                    printf("print\n");
-                    break;
-            }
-        } while(1);
+        dashboard(void);
     }
 
     else if(choiceOfMode == commandline) {
@@ -124,4 +123,48 @@ void PrintMainBoard(void)
     printf("**** Command line calculator ****\n\n");
     printf(" \t0- quit\n \t1- Scientific calculator\n \t2- Matrix calculator\n \t3- Complex number Calculator\n"
             "\t4- Clear screen\n \t5- Print Opeation code dashboard\n\n");
+}
+
+void dashboard(void)
+{
+    int code;
+
+    do {
+            printf("What do you want to do(choose from the operation codes): ");
+
+            if(scanf("%d", &code) != 1) { /* consume bad command */
+                while(getchar() != '\n')
+                    ;
+                continue;
+            }
+
+
+            switch(code) {
+                case 0:
+                    printf("quit\n");
+                    exit(EXIT_SUCCESS);
+                case 1:
+                    choice = scientific;
+                    printf("scientific lib\n");
+                    break;
+                
+                case 2:
+                    printf("Matrixlib\n");
+                    hoice = matrix;
+                    break;
+
+                case 3:
+                    printf("complex lib\n");
+                    choice = complex;
+                    break;
+
+                case 4:
+                    printf("clear\n");
+                    break;
+
+                case 5:
+                    printf("print\n");
+                    break;
+            }
+        } while(1);
 }
