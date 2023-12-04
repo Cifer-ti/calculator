fa54d6e313a6a9cc598a416a9473427cb2c2ec45
Cifer-ti <draculemihawk232@gmail.com>
2023-12-04 21:49:41 +0100

    Changed implementation of command line parsing
    Now using the argp library, for more efficient and unix like
    command line option parsing.

--- src/calc.c
+++ src/calc.c
@@ -24,42 +24,30 @@ typedef enum {
 }Mode;
 
 void PrintMainBoard(void);
-void dashboard(int *choice);
+void dashboard(void);
 
-static struct argp_option options[] = {
-    {"sci", 's', 0, 0, "Call the Scientific library", 0},
-    {"matx", 'm', 0, 0, "Perform matrix operations", 0},
-    {"compx", 'c', 0, 0, "Perform operations on complex numbers", 0},
-    {0}
-};
-
-// Parse the command-line arguments
-static error_t parse_opt(int key, char *arg) {
-    //struct arguments *arguments = state->input;
-
-    switch (key) {
+static int parse_opt(int key, char *arg, struct argp_state *state)
+{
+    switch(key) {
         case 's':
             choice = scientific;
             break;
+        
         case 'm':
             choice = matrix;
             break;
+
         case 'c':
             choice = complex;
             break;
-        case ARGP_KEY_ARG:
-            // Handle non-option arguments here, if needed
-            break;
-        default:
-            return ARGP_ERR_UNKNOWN;
     }
 
     return 0;
 }
 
+
 int main(int argc, char **argv)
 {
-    int choice;
     Mode choiceOfMode;
 
     printf("argc: %d\n", argc);
@@ -72,49 +60,30 @@ int main(int argc, char **argv)
     printf("%d\n", choiceOfMode);
     if(choiceOfMode == interactive) {
         PrintMainBoard();
-        dashboard(void);
+        dashboard();
     }
 
     else if(choiceOfMode == commandline) {
-        int opt;
 
-        struct option commandlineOptions[] = {
-            {"sci", no_argument, NULL, 's'},
-            {"matx", no_argument, NULL, 'm'},
-            {"compx", no_argument, NULL, 'c'},
-            {"help", no_argument, NULL, 'h'},
-            {NULL, 0, NULL, 0},
+        static struct argp_option options[] = {
+            {"sci", 's', 0, 0, "perform normal operaions"},
+            {"matx", 'm', 0, 0, "Perform matrix operations"},
+            {"compx", 'c', 0, 0, "Perform operations on complex numbers"},
+            {0}
         };
 
-        while((opt = getopt_long(argc, argv, "smch", commandlineOptions, NULL)) != -1) {
-            switch(opt) {
-                case 's':
-                case 'S':
-                    printf("sic\n");
-                    choice = scientific;
-                    break;
+        struct argp argp = {options, parse_opt};
 
-                case 'm':
-                    printf("moat\n");
-                    choice = matrix;
-                    break;
-                
-                case 'c':
-                    printf("com\n");
-                    choice = complex;
-                    break;
+        argp_parse(&argp, argc, argv, 0, 0, 0);
+    }
 
-                case 'h':
-                case 'H':
-                    printf("help\n");
-                    choice = help;
-                    break;
-
-                case '?':
-                    fprintf(stderr, "Try 'ls --help' for more information.\n");
-                    break;
-            }
-        }
+    switch(choice) {
+        case scientific:
+            scimain();
+            break;
+        case matrix:
+            matxmain();
+            break;
     }
 }
 
@@ -146,25 +115,25 @@ void dashboard(void)
                 case 1:
                     choice = scientific;
                     printf("scientific lib\n");
-                    break;
+                    return;
                 
                 case 2:
                     printf("Matrixlib\n");
-                    hoice = matrix;
-                    break;
+                    choice = matrix;
+                    return;
 
                 case 3:
                     printf("complex lib\n");
                     choice = complex;
-                    break;
+                    return;
 
                 case 4:
                     printf("clear\n");
-                    break;
+                    return;
 
                 case 5:
                     printf("print\n");
-                    break;
+                    return;
             }
         } while(1);
 }
